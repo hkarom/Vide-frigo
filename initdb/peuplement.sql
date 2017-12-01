dc16033ff44bd588916f71e7bf4f1af26ec6ac6b
Annegn <gningannemarie@gmail.com>
2017-12-01 19:51:57 +0100

    Update peuplement.sql

--- initdb/peuplement.sql
+++ initdb/peuplement.sql
@@ -1,6 +1,6 @@
 /**INSERT**/
 /*insertion dans la tabe inréingredient*/
-INSERT INTO ingredients (nom_ingredient) VALUES ('nom_ingredient');
+INSERT INTO ingredients (nom_ingredient) VALUES ('nom_ing');
 /*insertion dans la table recette*/
 INSERT INTO recettes ( nom_recette, description_recette, photo_recette, temps_cuisson, temps_prepa, etapes, catégorie,id_user)
 VALUES ( 'nom_rec', 'desc_rec ', 'photo_rec', '04:07:00', '06:07:00', 'etape-rec', 'PLAT','id_us');
@@ -31,7 +31,11 @@ SELECT * FROM avis WHERE id_user = 'varmembre';
 
 /**UPDATE**/
 UPDATE users SET login="", password="",age="",apropos="",photo="" WHERE id_user="id_us";
+<<<<<<< HEAD
+UPDATE recettes SET nom_recette="", description_recette="", photo_recette="", temps_cuisson="", temps_prepa="", etapes="", catégorie,id_user="" WHERE id_recette="id_rec";
+=======
 UPDATE recettes SET nom_recette="", description_recette="", photo_recette, temps_cuisson="", temps_prepa="", etapes="", catégorie,id_user="" WHERE id_recette="id_rec";
+>>>>>>> e68fe68dd9188c4840d99c7c94adc08066f66189
 UPDATE avis SET message="", etoile="" WHERE id_avis="id_av";
 UPDATE ingredients SET nom_ingredient="" WHERE id_ingredient="id_ing";
 
