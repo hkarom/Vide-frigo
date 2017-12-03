f15655927860024cdf1cffa9011ea662f4903b74
SalebHet <clement.nerestan@gmail.com>
2017-12-03 15:53:21 +0100

    Debut mise en place swagger-jsdoc

--- src/api/routes/recipe.js
+++ src/api/routes/recipe.js
@@ -3,17 +3,24 @@ let bd = require('./db');
 let router = express.Router();
 
 
-
+function treatment(err, response, values,  rows) {
+	if(err) response.status(400).send(values);
+	else {
+		if (rows.length != 0) {
+			values.push({'result' : 'success', 'data' : rows});
+		} else {
+			values.push({'result' : 'error', 'msg' : 'No Results Found'});
+		}
+		response.setHeader('Content-Type', 'application/json');
+		response.status(200).send(JSON.stringify(values));
+	}
+};
 
 router.get('/recipes/:userID', (req, res) => {
-	res.setHeader('Content-Type', 'application/json');
-		bd.query('SELECT * FROM Recipe  ....',[req.params.userID], (err,result) => {
-				if(err) throw err;
-				console.log(result);
+		bd.query('SELECT * FROM Recipe  ....',[req.params.userID], (err,cols) => {
+			let values = [];
+			treatment(err,res,values,cols)
 		})
-	}
-		//res.send(200, JSON.stringify(values));
-	}
 });
 
 router.post('/recipes/:userID', (req, res) => {
