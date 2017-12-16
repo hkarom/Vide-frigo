ff8eda20a56a61119fe5458c88f341f4707d2a7c
SalebHet <clement.nerestan@gmail.com>
2017-12-16 20:06:37 +0100

    Première version script peuplement BD JAVA

---
+++ vide_frigo/initdb/peuplement_BD/src/main/java/Main.java
@@ -0,0 +1,43 @@
+import src.source.*;
+import java.sql.*;
+import java.util.ArrayList;
+public class Main {
+	
+	public static void addRecipe(Recipe r, Connection conn) {
+		// the mysql insert statement
+	      String query = " insert into Recipe (id_creator, name, picture, cooking_time, steps, category)"
+	        + " values (?, ?, ?, ?, ?,?)";
+
+	      // create the mysql insert preparedstatement
+	      PreparedStatement preparedStmt = conn.prepareStatement(query);
+	      preparedStmt.setInt (1, 0);
+	      preparedStmt.setString (2, r.getName());
+	      preparedStmt.setBlob(3, new Blob()); 
+	      preparedStmt.setInt(4, Integer.parseInt(r.getPrepTime()));
+	      preparedStmt.setString (5, r.getDetail());
+	      preparedStmt.setString(6, "STARTER");
+
+	      // execute the preparedstatement
+	      preparedStmt.execute();
+	}
+	
+	public static void main() {
+		try {
+		// create a mysql database connection
+	      String myDriver = "org.gjt.mm.mysql.Driver";
+	      String myUrl = "jdbc:mysql://localhost/vide_frigo";
+	      Class.forName(myDriver);
+	      Connection conn = DriverManager.getConnection(myUrl, "root", "");
+	      ArrayList<Recipe>recipes = Recipe.search("tomate");
+	      for (Recipe r : recipes) {
+	    	  addRecipe(r,conn);
+	      }
+		
+	      conn.close();
+		}
+		catch (Exception e){
+			System.err.println(e.getMessage());
+		}
+		
+	}
+}
